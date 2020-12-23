03ea41eed98d8fb4beef5a4086a34df2295e2a3b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-12-23 00:58:31 -0300

    iniciando a implementação do clientesController.js

--- src/app/controllers/clientesController.js
+++ src/app/controllers/clientesController.js
@@ -1,1 +1,15 @@
-const clientesService = require('../service/');
+const clientesService = require('../service/clientesService');
+
+exports.getListClientes = (req, res) => {
+  clientesService.listAllClientes((err, clientes) => {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(clientes);
+  });
+};
+
+exports.getCliente = (req, res) => {
+  clientesService.listCliente(req.params.id, (err, cliente) => {
+    if (err) res.status(500).send(err);
+    elseres.status(200).json(cliente);
+  });
+};
