72a31d659cadb6cb8719377d9f9dc9426fb1d024
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-12-30 15:25:22 -0300

    finalizing the controller client

--- src/app/controllers/clientesController.js
+++ src/app/controllers/clientesController.js
@@ -10,6 +10,27 @@ exports.getListClientes = (req, res) => {
 exports.getCliente = (req, res) => {
   clientesService.listCliente(req.params.id, (err, cliente) => {
     if (err) res.status(500).send(err);
-    elseres.status(200).json(cliente);
+    else res.status(200).json(cliente);
+  });
+};
+
+exports.post = (req, res) => {
+  clientesService.insert(req.body, (err, cadastro) => {
+    if (err) res.status(500).send(err);
+    else res.status(201).json(cadastro);
+  });
+};
+
+exports.delete = (req, res) => {
+  clientesService.delete(req.params.id, (err) => {
+    if (err) res.status(500).send(err);
+    else res.status(200).send();
+  });
+};
+
+exports.put = (req, res) => {
+  clientesService.put(req.params.id, req.body, (err) => {
+    if (err) res.status(500).send(err);
+    else res.status(200).send();
   });
 };
