a0e73727a793525650b11bdfb88bc000663515de
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-12-30 15:25:55 -0300

    finalizing the routes client

--- src/app/routers/routes.js
+++ src/app/routers/routes.js
@@ -1,5 +1,11 @@
 const routes = require('express').Router();
+const clientesController = require('../controllers/clientesController');
+const post = require('./../controllers/post');
 
-routes.get('/');
+routes.get('/clientes', clientesController.getListClientes);
+routes.get('/cliente/:id', clientesController.getCliente);
+routes.post('/clientes', clientesController.post);
+routes.delete('/clientes/:id', clientesController.delete);
+routes.put('/clientes/:id', clientesController.put);
 
 module.exports = routes;
