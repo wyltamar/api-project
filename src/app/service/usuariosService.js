c9940316493d2a3916846f112b7edc477a12a91a
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-04 23:06:27 -0300

    crud initial

--- src/app/service/usuariosService.js
+++ src/app/service/usuariosService.js
@@ -3,3 +3,11 @@ const usuariosDao = require('../dao/usuariosDao');
 exports.listAllUsers = (callback) => {
   usuariosDao.listAllUsers(callback);
 };
+
+exports.listUser = (callback) => {};
+
+exports.insert = (novoUsuario, callback) => {};
+
+exports.delete = (id, callback) => {};
+
+exports.put = (id, alterRegistro, callback) => {};
