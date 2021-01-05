f7db662a2c1e1fd43e597b511fd1e045e1fd8190
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-04 23:20:54 -0300

    created listUser

--- src/app/dao/usuariosDao.js
+++ src/app/dao/usuariosDao.js
@@ -1,3 +1,4 @@
+const { insert } = require('../service/usuariosService');
 const connectionFactory = require('./connectionFactory');
 
 exports.listAllUsers = (callback) => {
@@ -11,6 +12,22 @@ exports.listAllUsers = (callback) => {
         connection.release();
         if (err) callback(err);
         else callback(err, usuarios);
+      });
+    }
+  });
+};
+
+exports.listUser = (callback) => {
+  connectionFactory.getConnection(function (err, connection) {
+    if (err) {
+      if (connection) connection.release();
+      callback(err);
+    } else {
+      let sql = 'select * from usuarios where id = ?';
+      connection.query(sql, [id], (err, usuario) => {
+        connection.release();
+        if (err) callback(err);
+        else callback(err, usuario);
       });
     }
   });
