9ddba42aaf4eaf8aa5337cd16331cbe459d556a4
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-05 21:49:58 -0300

    add endpoints of users

--- src/app/routers/routes.js
+++ src/app/routers/routes.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const clientesController = require('../controllers/clientesController');
+const usuariosController = require('../controllers/usuariosController');
 const post = require('./../controllers/post');
 
 routes.get('/clientes', clientesController.getListClientes);
@@ -7,5 +8,11 @@ routes.get('/cliente/:id', clientesController.getCliente);
 routes.post('/clientes', clientesController.post);
 routes.delete('/clientes/:id', clientesController.delete);
 routes.put('/clientes/:id', clientesController.put);
+
+routes.get('/usuarios', usuariosController.getListUsuarios);
+routes.get('/usuarios/:id', usuariosController.getUsuario);
+routes.post('/usuarios', usuariosController.post);
+routes.delete('/usuarios/:id', usuariosController.delete);
+routes.put('usuarios/:id', usuariosController.put);
 
 module.exports = routes;
