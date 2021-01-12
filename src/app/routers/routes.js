b9e24085a477d94484a64ddac02c08f1159b31dc
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-12 16:48:56 -0300

    correção do endpoint listar cliente

--- src/app/routers/routes.js
+++ src/app/routers/routes.js
@@ -4,7 +4,7 @@ const usuariosController = require('../controllers/usuariosController');
 const post = require('./../controllers/post');
 
 routes.get('/clientes', clientesController.getListClientes);
-routes.get('/cliente/:id', clientesController.getCliente);
+routes.get('/clientes/:id', clientesController.getCliente);
 routes.post('/clientes', clientesController.post);
 routes.delete('/clientes/:id', clientesController.delete);
 routes.put('/clientes/:id', clientesController.put);
