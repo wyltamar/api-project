c49ebe3eabcf3f195e3735b4f32284c24debc2ff
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-14 11:23:07 -0300

    initial created osDao.js

---
+++ src/app/dao/osDao.js
@@ -0,0 +1,35 @@
+const { isert } = require('../service/osService');
+const connectionFactory = require('./connectionFactory');
+
+exports.insert = (novaOs, callback) => {
+  connectionFactory.getConnection((err, connection) => {
+    if (err) {
+      if (connection) connection.release();
+      callback(err);
+    } else {
+      let sql =
+        'insert into tbos(numOs,dataOs,tipo,situacao,idCli,equipamento,defeito,servico,tecnico,valor) values(?,?,?,?,?,?,?,?,?,?)';
+      //prettier-ignore
+      connection.query(sql, [
+        novaOs.numOs,
+        novaOs.dataOs,
+        novaOs.tipo,
+        novaOs.situcao,
+        novaOs.idCli,
+        novaOs.equipamento,
+        novaOs.defeito,
+        novaOs.servico,
+        novaOs.tecnico,
+        novaOs.valor
+      ], (err,insertedrow)=>{
+        connection.release()
+        if(err) callback(err)
+        else{
+          novaOs.numOs = insertedrow.insertnumOs;
+          novaOs.dataOs = insertedrow.insertdataOs
+          callback(err,novaOs)
+        } 
+      })
+    }
+  });
+};
