f35d348b637c8801faf980594e0dbec75dbfd613
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-01-14 15:50:37 -0300

    creation of the  osController.js file

---
+++ src/app/controllers/osController.js
@@ -0,0 +1,8 @@
+const osService = require('../service/osService');
+
+exports.post = (req, res) => {
+  osService.insert(req.body, (err, novaOs) => {
+    if (err) res.status(500).send(err);
+    else res.status(201).jason(novaOs);
+  });
+};
