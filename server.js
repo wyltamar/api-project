f0f99be427d2aff87e9d899304b9999e65f78fc8
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-08-01 23:08:58 -0300

    update port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./src/config/custom_config');
 const { routes } = require('./src/config/custom_config');
 
-app.listen(3000, () => {
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
   console.log(`Servidor rodando no endereço: http://localhost:3000`);
 });
